25fb976061a2ca527f4d51e6103babcd8d664f62
Isaac Kuehnle-Nelson <ikethepike@outlook.com>
2020-10-03 21:57:48 +0200

    Scaffold out CardView component

---
+++ components/CardView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="card-view"></div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    cardsInView: {
+      type: Number,
+      default: 5,
+    },
+  },
+})
+</script>
