3dfff1eca1998082c94146983dfc7ee495675250
ikethepike <ikethepike@users.noreply.github.com>
2025-08-18 13:18:08 +0000

    Update countries.gen.json

--- types/vendor/factbook/factbook-types.gen.ts
+++ types/vendor/factbook/factbook-types.gen.ts
@@ -1,5 +1,5 @@
 
 
-  export const factbookRegions = ["Asia","Europe","Africa","Central America and the Caribbean","South America","Oceania","Middle East","Southeast Asia","North America","<strong>metropolitan France:</strong> Europe <p><strong>French Guiana:</strong> South America</p> <p><strong>Guadeloupe:</strong> Central America and the Caribbean</p> <p><strong>Martinique:</strong> Central America and the Caribbean</p> <p><strong>Mayotte:</strong> Africa</p> <strong>Reunion:</strong> World","Arctic Region","AsiaEurope"] as const 
+  export const factbookRegions = ["Asia","Europe","Africa","Central America and the Caribbean","South America","Oceania","Middle East","Southeast Asia","North America","<strong>metropolitan France:</strong> Europe <br><br><strong>French Guiana:</strong> South America <br><br><strong>Guadeloupe:</strong> Central America and the Caribbean <br><br><strong>Martinique:</strong> Central America and the Caribbean <br><br><strong>Mayotte:</strong> Africa <br><br><strong>Reunion:</strong> World","Arctic Region","AsiaEurope"] as const 
   export type FactbookRegion = typeof factbookRegions[number]
   
